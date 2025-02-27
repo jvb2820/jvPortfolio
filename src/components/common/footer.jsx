483a177703f8f4cb1acf213341efc93f2695256f
Jeuz Vinci Bas <jeuzvinci7913@gmail.com>
2025-02-27 11:34:46 +0800

    revert changes

--- src/components/common/footer.jsx
+++ src/components/common/footer.jsx
@@ -26,7 +26,7 @@ const Footer = () => {
 
 				<div className="footer-credits">
 					<div className="footer-credits-text">
-						© 2025@JVB. All Rights Reserve.
+						© 2025@JVB. All Rights Reserved.
 					</div>
 				</div>
 			</div>
